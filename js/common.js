f416b63e2384cc65537236eb6112c374741b2475
apospelov13 <small99@hotbox.ru>
2018-01-22 16:47:23 +0300

    v1

---
+++ js/common.js
@@ -0,0 +1,15 @@
+$(function() {
+  $('.slider').owlCarousel({
+		nav: false,
+		dots: false,
+    items: 3,
+    //autoHeight: true,
+    autoWidth:true,
+		margin: 10,
+    loop: true,
+    //autoplay: true,
+    //autoplayTimeout: 2000,
+    //autoplayHoverPause: true,
+  });
+  
+});
